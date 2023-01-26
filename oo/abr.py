8f64c2694ab90082acd0d1a06edd2834a5cb4e17
abrvanelli <mv.abrvanelli@gmail>
2023-01-26 14:16:36 -0600

    implementando Atualiza e filhos forzin de leve

--- oo/abr.py
+++ oo/abr.py
@@ -1,2 +1,9 @@
 class Atualiza:
-    pass
+    def cumprimentar(self):
+        return f'Ola {id(self)}'
+
+if __name__ == '__main__':
+    p = Atualiza()
+    print(Atualiza.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
